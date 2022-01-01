0b6c9345152b50124ea9183c1ac5f69a7c58f26d
Walter Goulet <wgoulet@gmail.com>
2022-01-01 20:55:45 +0000

    Added some design notes after reasoning through OAauth/OIDC flows

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import OAuth2Login from 'react-simple-oauth2-login';
 
 function App() {
+  const onSuccess = response => console.log(response);
+  const onFailure = response => console.error(response);
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +22,14 @@ function App() {
         >
           Learn React
         </a>
+        <OAuth2Login
+          authorizationUrl="https://dev-8623542.okta.com/oauth2/default/v1/authorize"
+          responseType="code"
+          scope="openid"
+          clientId="0oa3gtqcwmMDECDNa5d7"
+          redirectUri="http://localhost:3000/oauth-callback"
+          onSuccess={onSuccess}
+          onFailure={onFailure}/>
       </header>
     </div>
   );
